002b179815a4bfa4c70c0cfb345ce93439b8f7dc
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2019-01-07 14:13:30 +0100

    third change

--- src/SaveReadFile.java
+++ src/SaveReadFile.java
@@ -16,20 +16,24 @@ public class SaveReadFile {
             e.printStackTrace();
             shoppingList = new ArrayList<>();
         }
+
+
         List<Products>prodactList = new ArrayList<>();
             for (String s:shoppingList){
-
+                System.out.println(s);
+                
             String[]someData = s.split(":");
             prodactList.add(new Products(someData[0],
                     Integer.parseInt(someData[1]),
                     Double.parseDouble(someData[2])));
-        }
+            }
 
         float valueShopping = 0;
             for (Products product: prodactList){
                  valueShopping += product.getQuantiti()*product.getPrice();
-        }
+            }
         System.out.println("the purchase cost is: " + valueShopping + " zł ");
+
     }
 }
 
